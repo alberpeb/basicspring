dddefec7d491c98a79ce7428e66c5f42c861b647
alberpeb <alber.perezb@gmail.com>
2017-06-05 23:33:21 -0300

    Database and Table creation and filled

--- src/main/resources/script-database.sql
+++ src/main/resources/script-database.sql
@@ -1,10 +1,18 @@
 CREATE DATABASE ExampleDB;
 
-CREATE TABLE User ( 
+CREATE TABLE ´User´ ( 
 id INT NOT NULL AUTO_INCREMENT, 
 name VARCHAR(50), 
-Apellido VARCHAR(50), 
+surname VARCHAR(50), 
 email VARCHAR(100), 
 password BINARY(50), 
 
 PRIMARY KEY(id) );
+
+INSERT INTO ´User´ 
+( name, surname, email, password ) 
+VALUES
+( "Rick", "Sanchez", "wubbalubba@dubdub.com", "asdasd" ),
+( "Donald", "Trump", "masterexploder@america.com", "sweetdreamsaremadeofthis" ),
+( "Diego", "Maradona", "eeeeeeeee@d10go.com", "eeeeeeeeee" ),
+( "Richard", "Fort", "maeame@heavenfort.com", "enoughtguys" );
